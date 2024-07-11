c8bfcdf016de5295e312ea50def5b665ad66ddee
safonovoplatit <sazonovpeters@gmail.com>
2024-07-11 21:34:14 +0300

    improve install .sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # This script expects you to have apache2 installed and you being in the repo directory.
 
 
-rsync -aP --exclude=.gitignore --exclude=git* --exclude=install.sh ./* /var/www/html
+rsync -aP --exclude=.gitignore --cvs-exclude --exclude=install.sh ./* /var/www/html
